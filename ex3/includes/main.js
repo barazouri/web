3dfef365429a2a01ebfb691bb4608f12bf9a1457
Bar Azouri <barazouri24@gmail.com>
2018-05-20 19:23:22 +0300

    my commit

--- ex3/includes/main.js
+++ ex3/includes/main.js
@@ -1,12 +1,21 @@
 window.onload=function addSquare(){
-var plus=document.getElementById("plus");
+    var plus=document.getElementById("plus");
 }
 plus.onclick=function(){
-var nodeSection=document.createElement("section")
-nodeSection.style.backgroundColor="black";
-nodeSection.style.opacity=randomOpacity();
-nodeSection.style.cssFloat="left";
-document.getElementById("myMain").appendChild(nodeSection);
+    var nodeSection=document.createElement("section")
+    nodeSection.style.backgroundColor="black";
+    var opac=randomOpacity();
+    nodeSection.style.opacity=opac;
+    nodeSection.style.cssFloat="left";
+    nodeSection.onmouseover=(function(){
+        nodeSection.style.backgroundColor="white";
+        nodeSection.style.opacity=1;
+})
+    nodeSection.onmouseout=(function(){
+        nodeSection.style.backgroundColor="black";
+        nodeSection.style.opacity=opac;
+})
+    document.getElementById("myMain").appendChild(nodeSection);
 } 
 function randomOpacity(){
     
@@ -16,6 +25,3 @@ function randomOpacity(){
     return opac;
    
 }
-nodeSection.hover(function(){
-    nodeSection.style.backgroundColor="white";
-})
